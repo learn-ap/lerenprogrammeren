60b30d948e089e2c8a3211176e2807d09d545f92
Lode Bossuyt <lodebossuyt@hotmail.com>
2023-09-19 16:16:34 +0200

    19/09 oefeningen

--- oefenbundel/loops/oefening11/js/index.js
+++ oefenbundel/loops/oefening11/js/index.js
@@ -4,8 +4,6 @@
 // dus de actie word uitgevoerd, tot het niet verder dan de limiet dat je ingegeven heb
 
 
-
-
 let beginNr = 0;
 //let conditieGetal = Number(prompt("geef een getal?"));
 //test
@@ -14,14 +12,17 @@ let optelNr = 1;
 
 //for (let i = beginNr; i <= conditieGetal; i++) {
 for (let i = beginNr; i <= 7; i++) {
+    if (i<1){ //als het begin kleiner is dan in, in dit geval start i=0, dan voert hij het uit
+        document.write(`${beginNr}, ${optelNr}, `);
+    }
     let nieuweBeginNr = beginNr + optelNr;
-    if(i > 0){
+    if (i > 0) { //als het groter is dan nul, vult hij ', ' eerst voordat hij de 2de loop uitvoerd
         document.write(`, `);
     }
 //hier ga je de vorige var veranderen, zodat de loop begint met nieuwe cijfers
-    document.write(`${nieuweBeginNr} `);
-    beginNr=optelNr;
-    optelNr=nieuweBeginNr;
+    document.write(`${nieuweBeginNr}`);
+    beginNr = optelNr;
+    optelNr = nieuweBeginNr;
 }
 
 
---
+++ oefenbundel/loops/oefening12/js/index.js
@@ -0,0 +1,51 @@
+let getal1 = Number(prompt('Geef een getal'));
+let getal2 = Number(prompt('Geef een 2de getal'));
+
+for (let i = 1; i <= 30; i++) //die kleiner dan getal mss veranderen?
+{
+    som1 = getal1%i;
+    som2 = getal2%i;
+
+    if (som1>0) {
+        som1 = getal1%i;
+        som2 = getal2%i;
+    }
+}
+let ggd = som1 && som2;
+document.write(`De grootste GGD van ${getal1} & ${getal2} is ${ggd}`);
+
+// //poging 2
+// // //aftel manier
+// for (let i = getal1; i >=1; i--)
+// {
+//     som1 = getal1%i;
+//     som2 = getal2%i;
+//
+//     if (som1>0) {
+//         som1 = getal1%i;
+//         som2 = getal2%i;
+//     }
+// }
+//     let ggd = som1 && som2;
+//     document.write(`De grootste GGD van ${getal1} & ${getal2} is ${ggd}`);
+
+
+
+//poging 1
+//oude manier
+
+// let getal1 = Number(prompt('Geef een getal'));
+// let getal2 = Number(prompt('Geef een 2de getal'));
+//
+// for (let i = 1; i <= 15; i++)
+// {
+//     som1 = getal1%i;
+//     som2 = getal2%i;
+//
+//     if (som1>0) {
+//         som1 = getal1%i;
+//         som2 = getal2%i;
+//     }
+// }
+// let ggd = som1 && som2;
+// document.write(`De grootste GGD van ${getal1} & ${getal2} is ${ggd}`);
